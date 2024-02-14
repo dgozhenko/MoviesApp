beaa95859e4c4bae90e8c789958ec61974768415
dgozhenko <daniil.gozhenko@gmail.com>
2024-02-14 13:10:28 +0200

    Update UI

--- lib/presentation/widget/movie_list_screen/movie_list_widget.dart
+++ lib/presentation/widget/movie_list_screen/movie_list_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 import 'package:movies_app/domain/entity/movie_entity.dart';
+import 'package:movies_app/presentation/widget/movie_list_screen/movie_list_item.dart';
 import 'package:movies_app/util/converter/timestamp.dart';
 
 class MovieListWidget extends StatelessWidget {
@@ -20,54 +21,60 @@ class MovieListWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: movies.length,
-      itemBuilder: (context, index) {
-        final movie = movies[index];
-        final date = fromTimestamp(movie.creationTime);
-        final formattedDate = DateFormat.yMMMMd().format(date);
-
-        return Slidable(
-          key: Key(movies[index].id.toString()),
-          endActionPane: ActionPane(
-            extentRatio: 0.6,
-            motion: const ScrollMotion(),
-            children: [
-              SlidableAction(
-                onPressed: (context) {
-                  onWatchedTap(index);
-                },
-                flex: 1,
-                backgroundColor: Colors.blue,
-                foregroundColor: Colors.white,
-                icon: Icons.visibility,
-                label: movie.isWatched ? 'Unwatch' : 'Watch',
-              ),
-              SlidableAction(
-                onPressed: (context) {
-                  onDeleteTap(index);
-                },
-                backgroundColor: Colors.red,
-                foregroundColor: Colors.white,
-                icon: Icons.delete,
-                label: 'Delete',
-              ),
-            ],
-          ),
-          child: ListTile(
-            title: Text(
-              movie.title,
-              style: TextStyle(
-                decoration: movie.isWatched
-                    ? TextDecoration.lineThrough
-                    : TextDecoration.none,
-              ),
-            ),
-            subtitle: Text(formattedDate),
-            onTap: () => onMovieTap(index),
-          ),
-        );
-      },
+    return Container(
+      decoration: const BoxDecoration(color: Color(0xffefefef)),
+      child: ListView.builder(
+        itemCount: movies.length,
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        itemBuilder: (context, index) {
+          final movie = movies[index];
+          return Container(
+            margin: const EdgeInsets.symmetric(vertical: 8),
+            child: Slidable(
+                key: Key(movies[index].id.toString()),
+                endActionPane: ActionPane(
+                  extentRatio: 0.6,
+                  motion: const ScrollMotion(),
+                  children: [
+                    SlidableAction(
+                      onPressed: (context) {
+                        onWatchedTap(index);
+                      },
+                      flex: 1,
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(16),
+                        bottomLeft: Radius.circular(16),
+                      ),
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white,
+                      icon: Icons.visibility,
+                      label: movie.isWatched ? 'Unwatch' : 'Watch',
+                    ),
+                    SlidableAction(
+                      onPressed: (context) {
+                        onDeleteTap(index);
+                      },
+                      borderRadius: const BorderRadius.only(
+                          topRight: Radius.circular(16),
+                          bottomRight: Radius.circular(16)),
+                      backgroundColor: Colors.red,
+                      foregroundColor: Colors.white,
+                      icon: Icons.delete,
+                      label: 'Delete',
+                    ),
+                  ],
+                ),
+                child: MovieListItem(
+                    movie: movie,
+                    onCheckboxValueChanged: (_) {
+                      onWatchedTap(index);
+                    },
+                    onItemTap: () {
+                      onMovieTap(index);
+                    })),
+          );
+        },
+      ),
     );
   }
 }
