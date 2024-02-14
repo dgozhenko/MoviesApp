7f8b800903388fb5abadb662b9e2faa6008570b4
dgozhenko <daniil.gozhenko@gmail.com>
2024-02-14 13:43:53 +0200

    Add animations for adding/removing items

--- lib/presentation/widget/movie_list_screen/movie_list_widget.dart
+++ lib/presentation/widget/movie_list_screen/movie_list_widget.dart
@@ -1,9 +1,9 @@
+import 'package:animated_list_plus/animated_list_plus.dart';
+import 'package:animated_list_plus/transitions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
-import 'package:intl/intl.dart';
 import 'package:movies_app/domain/entity/movie_entity.dart';
 import 'package:movies_app/presentation/widget/movie_list_screen/movie_list_item.dart';
-import 'package:movies_app/util/converter/timestamp.dart';
 
 class MovieListWidget extends StatelessWidget {
   final List<Movie> movies;
@@ -22,59 +22,63 @@ class MovieListWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(color: Color(0xffefefef)),
-      child: ListView.builder(
-        itemCount: movies.length,
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        itemBuilder: (context, index) {
-          final movie = movies[index];
-          return Container(
-            margin: const EdgeInsets.symmetric(vertical: 8),
-            child: Slidable(
-                key: Key(movies[index].id.toString()),
-                endActionPane: ActionPane(
-                  extentRatio: 0.6,
-                  motion: const ScrollMotion(),
-                  children: [
-                    SlidableAction(
-                      onPressed: (context) {
-                        onWatchedTap(index);
-                      },
-                      flex: 1,
-                      borderRadius: const BorderRadius.only(
-                        topLeft: Radius.circular(16),
-                        bottomLeft: Radius.circular(16),
-                      ),
-                      backgroundColor: Colors.blue,
-                      foregroundColor: Colors.white,
-                      icon: Icons.visibility,
-                      label: movie.isWatched ? 'Unwatch' : 'Watch',
+        decoration: const BoxDecoration(color: Color(0xffefefef)),
+        child: ImplicitlyAnimatedList<Movie>(
+          items: movies,
+          areItemsTheSame: (oldItem, newItem) => oldItem.id == newItem.id,
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          itemBuilder: (context, animation, movie, index) {
+            return SizeFadeTransition(
+              sizeFraction: 0.7,
+              curve: Curves.easeInOut,
+              animation: animation,
+              child: Container(
+                key: Key(movie.id.toString()),
+                margin: const EdgeInsets.symmetric(vertical: 8),
+                child: Slidable(
+                    endActionPane: ActionPane(
+                      extentRatio: 0.6,
+                      motion: const ScrollMotion(),
+                      children: [
+                        SlidableAction(
+                          onPressed: (context) {
+                            onWatchedTap(index);
+                          },
+                          flex: 1,
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(16),
+                            bottomLeft: Radius.circular(16),
+                          ),
+                          backgroundColor: Colors.blue,
+                          foregroundColor: Colors.white,
+                          icon: Icons.visibility,
+                          label: movie.isWatched ? 'Unwatch' : 'Watch',
+                        ),
+                        SlidableAction(
+                          onPressed: (context) {
+                            onDeleteTap(index);
+                          },
+                          borderRadius: const BorderRadius.only(
+                              topRight: Radius.circular(16),
+                              bottomRight: Radius.circular(16)),
+                          backgroundColor: Colors.red,
+                          foregroundColor: Colors.white,
+                          icon: Icons.delete,
+                          label: 'Delete',
+                        ),
+                      ],
                     ),
-                    SlidableAction(
-                      onPressed: (context) {
-                        onDeleteTap(index);
-                      },
-                      borderRadius: const BorderRadius.only(
-                          topRight: Radius.circular(16),
-                          bottomRight: Radius.circular(16)),
-                      backgroundColor: Colors.red,
-                      foregroundColor: Colors.white,
-                      icon: Icons.delete,
-                      label: 'Delete',
-                    ),
-                  ],
-                ),
-                child: MovieListItem(
-                    movie: movie,
-                    onCheckboxValueChanged: (_) {
-                      onWatchedTap(index);
-                    },
-                    onItemTap: () {
-                      onMovieTap(index);
-                    })),
-          );
-        },
-      ),
-    );
+                    child: MovieListItem(
+                        movie: movie,
+                        onCheckboxValueChanged: (_) {
+                          onWatchedTap(index);
+                        },
+                        onItemTap: () {
+                          onMovieTap(index);
+                        })),
+              ),
+            );
+          },
+        ));
   }
 }
